40b3f58d7903cb5b0a449d5565f7ce09dd9db517
root <fr3shnuts@gmail.com>
2018-06-10 14:00:31 -0700

    all-in-one

---
+++ combined.php
@@ -0,0 +1,50 @@
+<!doctype html>
+
+<head>
+<title> "Upload Form" </title>
+</head>
+
+<body>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+      <input type="file" name="file"><br><br>
+      <input type="submit" value="Submit">
+</form>
+
+<body>
+</html>
+
+<?php
+
+// NOT SAFE for public use. Just use as examples.
+
+// Create 2 variables:
+// 1. Upload Directory.
+// 2. Introduce path prefix to "test.txt"; "/uploads/test.txt"
+// 3. $temp - saves the temporary file name.
+// ============================================================
+
+$path = "uploads/";
+$uploadfile = $path.basename($_FILES['file']['name']);
+$temp = $_FILES['file']['tmp_name'];
+
+
+// Debug Information
+// =================
+
+echo "File Statistics<br>=========<br>";
+echo "Name: ".$_FILES['file']['name']."<br>";
+echo "Size: ".$_FILES['file']['size']."<br>";
+echo "Type: ".$_FILES['file']['type']."<br>";
+echo "Temp: ".$_FILES['file']['tmp_name']."<br>";
+
+
+// Check file upload status.
+// =========================
+if (move_uploaded_file($temp, $uploadfile)) {
+    echo "The file has been uploaded";
+} else {
+    echo "There was an error uploading the file, please try again!";
+}
+
+?>
+
